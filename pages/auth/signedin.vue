9514ba9bf5f1e59653a3e5ced2bf238dc17da279
James Paik <jpaik456@gmail.com>
2020-06-08 21:20:22 -0400

    Styled signing in page

--- pages/auth/signedin.vue
+++ pages/auth/signedin.vue
@@ -1,3 +1,10 @@
 <template>
-  <p>Signing in...</p>
+  <div class="container">
+    <div
+      class="row my-5 h-100 w-100 justify-content-center align-items-center flex-column"
+    >
+      <b-spinner variant="primary" label="Signing in..."></b-spinner>
+      <h2 class="my-2">Signing In...</h2>
+    </div>
+  </div>
 </template>
